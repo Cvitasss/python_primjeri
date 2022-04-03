0a4fd18dcd96cf4866f29319cf5a2e58bad9c284
Cvitasss <102925917+Cvitasss@users.noreply.github.com>
2022-04-03 22:33:01 +0200

    Create Cvijet.py

---
+++ Cvijet.py
@@ -0,0 +1,10 @@
+from turtle import*
+fillcolor("blue")
+begin_fill()
+for j in range(6):
+    begin_fill()
+    for i in range(5):
+        fd(100);rt(360/5)
+    fd(100);lt(360/6)
+    end_fill()
+  
